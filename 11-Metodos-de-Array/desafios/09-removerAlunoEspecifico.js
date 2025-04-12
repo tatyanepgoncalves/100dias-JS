f6558524ef76dcb922d1119256204284353fb9d4
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2025-04-12 18:52:55 -0300

    :sparkles: feat: Atualização em desafio de Métodos de Array

--- 11-Metodos-de-Array/desafios/09-removerAlunoEspecifico.js
+++ 11-Metodos-de-Array/desafios/09-removerAlunoEspecifico.js
@@ -1,5 +1,13 @@
 // 👉 **Desafio:** Use `.splice()` para remover **apenas o aluno 'Taty'** da lista.
 
 const turma = ['Ana', 'Rafa', 'Taty', 'Leo'];
-turma.splice(2, 'Taty')
-console.log(turma)
+const remover = turma.splice(2, 2, "Leo")
+console.log(`Turma: ${turma}`)
+
+
+
+// const myArray = [1, 2, 3, 4, 5];
+// const x = myArray.splice(1, 1);
+// console.log(`myArray values: ${myArray}`);
+// console.log(`variable x value: ${x}`);
+
